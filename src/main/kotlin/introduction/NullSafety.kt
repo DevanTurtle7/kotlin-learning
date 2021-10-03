88a3c60d310ad214a527c7cd29319f66d76d62eb
Devan Kavalchek <devankav7@gmail.com>
2021-10-03 17:23:26 -0400

    finished null safety

--- src/main/kotlin/introduction/NullSafety.kt
+++ src/main/kotlin/introduction/NullSafety.kt
@@ -4,6 +4,14 @@ fun strLength(notNull: String): Int {
     return notNull.length
 }
 
+fun describeString(maybeString: String?): String {
+    if (maybeString != null && maybeString.length > 0) {
+        return "String of length ${maybeString.length}"
+    } else {
+        return "Empty or null string"
+    }
+}
+
 fun main() {
     var neverNull: String = "This can't be null"
     //neverNull = null
@@ -16,4 +24,7 @@ fun main() {
 
     strLength(neverNull)
     //strLength(nullable)
+
+    println(describeString(nullable))
+    println(describeString("Hello there!"))
 }
