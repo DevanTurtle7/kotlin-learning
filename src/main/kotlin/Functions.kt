007a3bbd1124b52a8282a69c8322df1ce5ee2424
Devan Kavalchek <devankav7@gmail.com>
2021-10-03 16:58:06 -0400

    added infix functions and classes

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -12,6 +12,17 @@ fun sum(x: Int, y: Int): Int {
 
 fun multiply(x: Int, y: Int) = x * y
 
+infix fun Int.times(y: Int) = this * y
+infix fun String.onto(other: String) = Pair(this, other)
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+
+    infix fun likes(other: Person) {
+        likedPeople.add(other)
+    }
+}
+
 fun main() {
     printMessage("Hello")
     printMessageWithPrefix("Hello", "Log")
@@ -19,4 +30,13 @@ fun main() {
     printMessageWithPrefix(prefix = "Log", message = "Hello")
     println(sum(1, 2))
     println(multiply(2, 4))
+    println(2 times 4)
+    println("this" to "that")
+    println("McLaren" onto "Lucas")
+
+    val devan = Person("Devan")
+    val gil = Person("Gil")
+    devan likes gil
+    println(devan.name)
+    println(devan.likedPeople)
 }
